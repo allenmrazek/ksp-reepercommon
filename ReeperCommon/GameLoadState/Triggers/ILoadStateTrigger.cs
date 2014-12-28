debdabc767c7a203e7104f8c065d2dd75460ccd7
Allen Mrazek <amrazek@hotmail.com>
2014-12-27 17:50:44 -0800

    removed unnecessary delegate

--- ReeperCommon/GameLoadState/Triggers/ILoadStateTrigger.cs
+++ ReeperCommon/GameLoadState/Triggers/ILoadStateTrigger.cs
@@ -2,8 +2,6 @@
 
 namespace ReeperCommon.GameLoadState.Triggers
 {
-    public delegate void TriggerCallback(ILoadStateTrigger trigger, LoadStateMarker.State state);
-
     public interface ILoadStateTrigger
     {
         void UpdateState();
