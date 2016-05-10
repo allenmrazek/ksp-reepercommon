9b88525a476bc00b25f0fa29cd1347177252718a
Allen Mrazek <amrazek@hotmail.com>
2016-05-10 06:55:24 -0700

    Realized why dragging was stuttery (used own RectTransform as reference); renamed GUIcomponents namespace to UI

---
+++ ReeperCommon/UI/RectTransformDraggable.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace ReeperCommon.UI
+{
+    [DisallowMultipleComponent, RequireComponent(typeof(RectTransform))]
+    public class RectTransformDraggable : MonoBehaviour, IPointerDownHandler, IDragHandler
+    {
+        private RectTransform _resizedTarget;
+        private RectTransform _parent;
+        private Vector2 _dragBegin;
+        private Vector3 _originalAnchoredPosition;
+
+        private void Awake()
+        {
+            _resizedTarget = GetComponent<RectTransform>();
+        }
+
+        public void OnPointerDown(PointerEventData eventData)
+        {
+            if (_resizedTarget == null) return;
+
+            _parent = _resizedTarget.transform.parent as RectTransform;
+            if (_parent == null) return;
+
+            _originalAnchoredPosition = _resizedTarget.anchoredPosition;
+
+            RectTransformUtility.ScreenPointToLocalPointInRectangle(_parent, eventData.position,
+                eventData.pressEventCamera, out _dragBegin);
+        }
+
+
+        public void OnDrag(PointerEventData eventData)
+        {
+            if (_resizedTarget == null) return;
+            if (_parent == null) return;
+
+            Vector2 currentMousePosition;
+
+            if (!RectTransformUtility.ScreenPointToLocalPointInRectangle(_parent, eventData.position,
+                eventData.pressEventCamera, out currentMousePosition)) return;
+
+            Vector3 offset = currentMousePosition - _dragBegin;
+
+            _resizedTarget.anchoredPosition = _originalAnchoredPosition + offset;
+        }
+    }
+}
--- ReeperCommon/UI/RectTransformResizerHandle.cs
+++ ReeperCommon/UI/RectTransformResizerHandle.cs
@@ -1,11 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.EventSystems;
 
-namespace ReeperCommon.GUIComponents
+namespace ReeperCommon.UI
 {
     [DisallowMultipleComponent, RequireComponent(typeof(RectTransform))]
     class RectTransformResizerHandle : MonoBehaviour, IPointerDownHandler, IDragHandler
@@ -20,39 +16,25 @@ namespace ReeperCommon.GUIComponents
         private Vector2 _dragStart;
         private Vector2 _originalSizeDelta;
 
-        private void Awake()
-        {
-            print("RectTransformResizerHandle.Awake");
-        }
         public void OnPointerDown(PointerEventData eventData)
         {
-            print("OnPointerDown");
             if (_resizableTarget == null) return;
 
             _originalSizeDelta = _resizableTarget.sizeDelta;
-            print("Original size delta: " + _originalSizeDelta);
-            print("Width: " + _resizableTarget.rect.width);
-            print("Height: " + _resizableTarget.rect.height);
 
             RectTransformUtility.ScreenPointToLocalPointInRectangle(_resizableTarget, eventData.position,
                 eventData.pressEventCamera, out _dragStart);
-
-            print("drag start: " + _dragStart);
         }
 
 
         public void OnDrag(PointerEventData eventData)
         {
-            print("OnDrag");
             if (_resizableTarget == null) return;
 
             Vector2 currentPointerLocation;
 
             RectTransformUtility.ScreenPointToLocalPointInRectangle(_resizableTarget, eventData.position,
                 eventData.pressEventCamera, out currentPointerLocation);
-
-            print("Current mouse: " + currentPointerLocation);
-            print("Drag start: " + _dragStart);
 
             var offset = currentPointerLocation - _dragStart;
 
@@ -63,10 +45,6 @@ namespace ReeperCommon.GUIComponents
             );
 
             _resizableTarget.sizeDelta = sizeDelta;
-            print("Current SizeDelta: " + _originalSizeDelta);
-            print("New SizeDelta: " + sizeDelta);
-
-            //print("SizeDelta: " + _resizableTarget.sizeDelta);
         }
     }
 }
